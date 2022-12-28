f80d7606a89247554bf13d1ed2c0f38eb94fd505
bhure <bhure@LAPTOP-9D514ALF>
2022-12-28 12:56:38 +0530

    project

---
+++ src/test/java/scripts/Tc5.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc5 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello Shraddha",true);
+	}
+}
